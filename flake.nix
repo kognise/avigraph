b9c8758f1dad22912523c06d90d3b9430dc400ea
superwhiskers <whiskerdev@protonmail.com>
2021-08-18 16:09:50 -0500

    avigraph: add nix development shell files for use with nix-direnv

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "development shell for avigraph";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        devShell = pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustup ]; };
+      });
+}
